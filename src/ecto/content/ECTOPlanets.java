0dbb13b025d5f722adb8ad5c4fdc5175bb8dc035
AFARIUS <afarius.private@gmail.com>
2024-01-07 19:08:47 +0300

    update our planet code

--- src/ecto/content/ECTOPlanets.java
+++ src/ecto/content/ECTOPlanets.java
@@ -1,13 +1,23 @@
 package ecto.content;
 
+import arc.graphics.Color;
+import mindustry.graphics.g3d.HexMesh;
+import mindustry.graphics.g3d.HexSkyMesh;
+import mindustry.graphics.g3d.MultiMesh;
 import mindustry.type.Planet;
+import mindustry.content.Planets;
 
 public class ECTOPlanets {
     public static Planet
     ectorum;
 
     public static void load(){
-        ectorum = new Planet("ectorum"){{
+        ectorum = new Planet("ectorum", Planets.sun, 1f, 3){{
+            meshLoader = () -> new HexMesh(this, 8);
+            cloudMeshLoader = () -> new MultiMesh(
+                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+            );
 
             allowLaunchToNumbered = false;
         }};
