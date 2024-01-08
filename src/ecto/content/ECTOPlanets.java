0206e46a7d8b583da14b2fbf6ba1dc7daeef5ba7
NRE <NRE@NULLREFEXC>
2024-01-08 22:26:12 +0300

    fix crash

--- src/ecto/content/ECTOPlanets.java
+++ src/ecto/content/ECTOPlanets.java
@@ -5,6 +5,8 @@ import mindustry.graphics.g3d.HexMesh;
 import mindustry.graphics.g3d.HexSkyMesh;
 import mindustry.graphics.g3d.MultiMesh;
 import mindustry.graphics.g3d.HexMesher;
+import mindustry.maps.planet.ErekirPlanetGenerator;
+import mindustry.maps.planet.SerpuloPlanetGenerator;
 import mindustry.type.Planet;
 import mindustry.content.Planets;
 
@@ -14,15 +16,15 @@ public class ECTOPlanets {
 
     public static void load(){
         ectorum = new Planet("ectorum", Planets.sun, 1f, 3){{
-            /*meshLoader = () -> new HexMesh(this, 6);
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
             cloudMeshLoader = () -> new MultiMesh(
                     new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
                     new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
-            );*/
+            );
             alwaysUnlocked = true;
             atmosphereColor = Color.valueOf("79a89d");
             clearSectorOnLose = true;
-
             allowLaunchToNumbered = false;
         }};
     }
