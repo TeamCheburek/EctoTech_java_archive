efd46a62067686115fc2a9ee0e56952df382782e
AFARIUS <afarius.private@gmail.com>
2024-01-08 21:59:32 +0300

    updated ECTOPlanets.java (commented meshLoader)

--- src/ecto/content/ECTOPlanets.java
+++ src/ecto/content/ECTOPlanets.java
@@ -4,6 +4,7 @@ import arc.graphics.Color;
 import mindustry.graphics.g3d.HexMesh;
 import mindustry.graphics.g3d.HexSkyMesh;
 import mindustry.graphics.g3d.MultiMesh;
+import mindustry.graphics.g3d.HexMesher;
 import mindustry.type.Planet;
 import mindustry.content.Planets;
 
@@ -18,6 +19,9 @@ public class ECTOPlanets {
                     new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
                     new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
             );*/
+            alwaysUnlocked = true;
+            atmosphereColor = Color.valueOf("79a89d");
+            clearSectorOnLose = true;
 
             allowLaunchToNumbered = false;
         }};
